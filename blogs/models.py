8ec9fb03490ea4f1052cab84f6cbee13f279b195
yogeshmanghnani <yamanghnani@gmail.com>
2020-02-29 09:42:54 +0530

    Altered category title

--- blogs/models.py
+++ blogs/models.py
@@ -36,7 +36,7 @@ class Comment(models.Model):
 
 
 class Category(models.Model):
-	title = models.CharField(max_length=128, unique=True)
+	title = models.CharField(max_length=1024, unique=True)
 	desc = models.CharField(max_length=2048)
 	parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name="children")
 
