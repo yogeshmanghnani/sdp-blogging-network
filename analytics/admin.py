7cfcaa18b0cb784da7c294530edd234536085ac6
yogeshmanghnani <yamanghnani@gmail.com>
2020-02-29 09:41:04 +0530

    Added css and links and proxy models

--- analytics/admin.py
+++ analytics/admin.py
@@ -10,7 +10,7 @@ class ViewOnlyAdmin(admin.ModelAdmin):
 		return False
 
 	def has_delete_permission(self, request, obj=None):
-		return False
+		return True
 
 	def has_change_permission(self, request, obj=None):
 		return False
