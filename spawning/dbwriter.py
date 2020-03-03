1a60b6e17d7aca3a1451f913f102fa32018d04a3
yogeshmanghnani <yamanghnani@gmail.com>
2020-03-03 22:59:03 +0530

    Removed spawning shit

--- spawning/dbwriter.py
+++
@@ -1,9 +0,0 @@
-from . import dbreader
-from users.models import User
-from blogs.models import Blog_Post
-
-
-u = User.objects.all()[5]
-for i in dbreader.rows:
-	b = Blog_Post(title = i[1], content = i[2], author = u)
-	b.save()
